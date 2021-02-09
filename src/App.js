0e4e8a1a98fee6f39fbe329ca0fd38ca837d834d
Neydor Avila <navila@fivetech.co>
2021-02-09 14:32:07 -0500

    cambios del lunes noche

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import MapaTulua from './MapaTulua';
+import Login from './Login';
+import Material from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Gasolineras de Tulua</h1>
+        <Login />
         <MapaTulua />
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from '@material-ui/core/Button'
 
 ReactDOM.render(
   <React.StrictMode>
