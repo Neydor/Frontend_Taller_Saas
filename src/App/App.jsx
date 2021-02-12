74d6e331d3b7001a1bb1b2890596d721b02e7d38
Neydor Avila <navila@fivetech.co>
2021-02-11 23:59:15 -0500

    cambios entrega

---
+++ src/App/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Router, Route, Link } from 'react-router-dom';
+
+import { history } from '@/_helpers';
+import { authenticationService } from '@/_services';
+import { PrivateRoute} from '@/_components';
+import { HomePage } from '@/HomePage';
+import { LoginPage } from '@/LoginPage';
+//import MapaTulua from '../_components/MapaTulua'
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: null
+        };
+    }
+
+    componentDidMount() {
+        authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
+    }
+
+    logout() {
+        authenticationService.logout();
+        history.push('/login');
+    }
+
+    render() {
+        const { currentUser } = this.state;
+        return (
+            <Router history={history}>
+                <div>
+                    {currentUser &&
+                        <nav className="navbar navbar-expand navbar-dark bg-dark">
+                            <div className="navbar-nav">
+                                <Link to="/" className="nav-item nav-link">Mapa</Link>
+                                <a onClick={this.logout} className="nav-item nav-link">Cerrar sesion</a>
+                                <button  className="nav-item nav-link disable pull-right">{currentUser.result[0].Correo}</button>
+                            </div>
+                        </nav>
+                    }
+                    <div className="jumbotron">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-6 offset-md-3">
+                                    <PrivateRoute exact path="/" component={HomePage} />
+                                    {/* <PrivateRoute exact path="/" component={MapaTulua} /> */}
+                                    <Route path="/login" component={LoginPage} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export { App }; 
